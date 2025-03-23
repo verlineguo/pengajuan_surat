3c6f9c6b62b443f9cc84bbaedfef84cf302735b3
Elika N <153571556+blissels@users.noreply.github.com>
2025-03-23 12:02:29 +0700

    remove copies

--- resources/views/TU/layouts/footer.blade.php
+++ resources/views/TU/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer px-4">
-    <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io/product/bootstrap-dashboard-template/">Bootstrap Admin Template</a> © 2025 creativeLabs.</div>
-    <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI PRO UI Components</a></div>
+    <div><a href="https://coreui.io">Pengajuan Surat </a><a href="https://coreui.io/product/bootstrap-dashboard-template/"></a> © 2025 pemograman web lanjut.</div>
+    <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">2372011, 2372901</a></div>
 </footer>
