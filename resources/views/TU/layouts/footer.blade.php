8fd217b0be295c55bed5a66f63ffda59ef90adce
Elika N <153571556+blissels@users.noreply.github.com>
2025-03-23 12:04:26 +0700

    .

--- resources/views/TU/layouts/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer px-4">
-    <div><a href="https://coreui.io">Pengajuan Surat </a><a href="https://coreui.io/product/bootstrap-dashboard-template/"></a> © 2025 pemograman web lanjut.</div>
-    <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">2372011, 2372901</a></div>
-</footer>
